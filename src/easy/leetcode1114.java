83369fd16471290a84c336c18a8935bc05f864ca
elon shi jun yan <chinachina@Yan>
2020-07-06 14:11:35 +0800

    master

--- src/easy/leetcode1114.java
+++ src/easy/leetcode1114.java
@@ -1,4 +1,30 @@
 package easy;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class leetcode1114 {
+    private AtomicInteger firstJobDone = new AtomicInteger(0);
+    private AtomicInteger secondJobDone = new AtomicInteger(0);
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        printFirst.run();
+        firstJobDone.incrementAndGet();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        while (firstJobDone.get() != 1) {
+
+        }
+        printSecond.run();
+        secondJobDone.incrementAndGet();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        while (secondJobDone.get() != 1) {
+
+        }
+        printThird.run();
+    }
+
 }
