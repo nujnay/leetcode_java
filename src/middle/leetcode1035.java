de3238b461621d1a7d7693485df1053663ba131d
elon shi jun yan <1292015395@qq.com>
2020-07-13 14:38:18 +0800

    master

--- src/middle/leetcode1035.java
+++ src/middle/leetcode1035.java
@@ -23,16 +23,14 @@ public class leetcode1035 {
     }
 
 
-    public static int maxU(int[] A,int[] B) {
-
-        int[][] dp = new int[A.length+1][B.length+1];
+    public static int test1(int[] A, int[] B) {
+        int[][] dp = new int[A.length + 1][B.length + 1];
         for (int i = 1; i < A.length; i++) {
             for (int j = 1; j < B.length; j++) {
-                dp[i][j] = A[i - 1] == B[j - 1] ? dp[i - 1][j - 1] + 1 :
-                        Math.max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = A[i - 1] == B[j - 1] ? dp[i - 1][j - 1] + 1 : Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
         return dp[A.length][B.length];
-    }
 
+    }
 }
