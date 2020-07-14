20da4b97863161477161ad1ac053a645095c4f03
elon shi jun yan <1292015395@qq.com>
2020-07-14 11:02:04 +0800

    master

--- src/week2/leetcode1367.java
+++ src/week2/leetcode1367.java
@@ -1,4 +1,18 @@
 package week2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class leetcode1367 {
+    Map<Integer, String> map = new HashMap<>();
+    int i = 0;
+
+    public String encode(String longUrl) {
+        map.put(i, longUrl);
+        return "http://tinyurl.com/" + i++;
+    }
+
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
 }
