645c2fd60aec1b2645f0d0d4104493c8d8b0b443
yanjun <yanjun@gmail.com>
2020-07-16 21:18:23 +0800

    master

--- src/array/leetcode0026.java
+++ src/array/leetcode0026.java
@@ -1,6 +1,10 @@
 package array;
 
 public class leetcode0026 {
+    public static void main(String[] args) {
+        int[] ints = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        removeDuplicates(ints);
+    }
 
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
