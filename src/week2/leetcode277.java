ca2c530d6cc04e0a64e349a023e3285a96c863ff
yanjun <yanjun@gmail.com>
2020-07-17 15:57:29 +0800

    master

--- src/week2/leetcode277.java
+++ src/week2/leetcode277.java
@@ -1,4 +1,25 @@
 package week2;
 
 public class leetcode277 {
+    public int findCelebrity(int n) {
+        int left = 0;
+        int right = n - 1;
+        while (left != right) {
+            if (knows(left, right)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i != left && (knows(left, i) || !knows(i, left))) {
+                return -1;
+            }
+        }
+        return left;
+    }
+
+    public static boolean knows(int a, int b) {
+        return true;
+    }
 }
