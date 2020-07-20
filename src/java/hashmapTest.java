f8e4714ca558c2f1181116f80006931ac9eaf5ea
yanjun <1292015395@qq.com>
2020-07-20 14:49:03 +0800

    master

---
+++ src/java/hashmapTest.java
@@ -0,0 +1,9 @@
+package java;
+
+import java.util.HashMap;
+
+public class hashmapTest {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+    }
+}
