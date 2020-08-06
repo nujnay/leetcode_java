c8b9c8732f899a838eca863e1102def428501ba0
yanjun <1292015395@qq.com>
2020-08-06 15:39:26 +0800

    master

--- src/leetcode/week5/leetcode0633.java
+++ src/leetcode/week5/leetcode0633.java
@@ -3,12 +3,12 @@ package leetcode.week5;
 public class leetcode0633 {
     /**
      * Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/sum-of-square-numbers
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
-    public static boolean  judgeSquareSum(int c) {
+    public static boolean judgeSquareSum(int c) {
         for (long a = 0; a * a <= c; a++) {
             int b = c - (int) (a * a);
             if (binary_search(0, b, b)) {
