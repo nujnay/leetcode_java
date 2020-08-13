b563508c014f9a4b22bfe5bd935f1fe2532d0c6b
yanjun <1292015395@qq.com>
2020-08-13 14:05:13 +0800

    master

--- src/leetcode/week6/leetcode0003.java
+++ src/leetcode/week6/leetcode0003.java
@@ -1,5 +1,8 @@
 package leetcode.week6;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given a string, find the length of the longest substring without repeating characters.
  *
@@ -10,5 +13,20 @@ package leetcode.week6;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class leetcode0003 {
-
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        int rk = -1;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                occ.add(s.charAt(rk + 1));
+            }
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
 }
