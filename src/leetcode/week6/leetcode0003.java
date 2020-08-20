50d481fa9827a8429dd3946881f18e09cce4c22f
yanjun <1292015395@qq.com>
2020-08-20 20:21:32 +0800

    master

--- src/leetcode/week6/leetcode0003.java
+++ src/leetcode/week6/leetcode0003.java
@@ -11,6 +11,15 @@ import java.util.Set;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * alright hey what'up guys they`re quite here to tech encoding stuff on twich and
+ * Youtube and yeah that's cool right
+ * this is the longest substring without repeating characters
+ * so this is given a string find the length of the longest substring
+ * substring meaning part of the string that don't have repeating characters
+ * so in this case you know this is a substring any this is a substring
+ *
+ *
  */
 public class leetcode0003 {
     public static int lengthOfLongestSubstring(String s) {
