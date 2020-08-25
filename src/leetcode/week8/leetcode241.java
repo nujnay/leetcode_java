77645079d9a93b23948bb024dc96da4dace5649f
yanjun <1292015395@qq.com>
2020-08-25 10:40:57 +0800

    master

--- src/leetcode/week8/leetcode241.java
+++ src/leetcode/week8/leetcode241.java
@@ -1,5 +1,13 @@
 package leetcode.week8;
 
+/**
+ * Different ways to add Parentheses
+ * Given s string of numbers and operateprs, return all possible results from computing all the different
+ * possible ways to group numbers and operators. The valid operators are +,- and *.
+ *
+ * Example 1:
+ *
+ */
 public class leetcode241 {
 
 }
