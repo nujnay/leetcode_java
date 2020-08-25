999ff08ddec6c5cc30e15844d9244ab73567732d
yanjun <1292015395@qq.com>
2020-08-25 11:41:34 +0800

    master

--- src/leetcode/week8/leetcode241.java
+++ src/leetcode/week8/leetcode241.java
@@ -6,6 +6,11 @@ package leetcode.week8;
  * possible ways to group numbers and operators. The valid operators are +,- and *.
  *
  * Example 1:
+ * Input: "2-1-1"
+ * Output: [0, 2]
+ * Explanation:
+ * ((2-1)-1) = 0
+ * (2-(1-1)) = 2
  *
  */
 public class leetcode241 {
