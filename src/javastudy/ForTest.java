2257ea2640eb63a193a31fc5f28034ed1ad7003b
yanjun <1292015395@qq.com>
2020-10-06 15:17:10 +0800

    master

--- src/javastudy/ForTest.java
+++ src/javastudy/ForTest.java
@@ -3,8 +3,9 @@ package javastudy;
 public class ForTest {
     public static void main(String[] args) {
         int a = 1;
-        for (int i = 1; i < 10; i = i * 2) {
+        for (String i = "1"; !i.equals("8"); i = String.valueOf(Integer.parseInt(i) * 2)) {
             System.out.println(i);
         }
+
     }
 }
