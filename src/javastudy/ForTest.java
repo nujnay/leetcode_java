eaeb64cebb3246aee01252a79434210837f0646b
yanjun <1292015395@qq.com>
2020-10-06 15:23:22 +0800

    master

--- src/javastudy/ForTest.java
+++ src/javastudy/ForTest.java
@@ -3,9 +3,25 @@ package javastudy;
 public class ForTest {
     public static void main(String[] args) {
         int a = 1;
-        for (String i = "1"; !i.equals("8"); i = String.valueOf(Integer.parseInt(i) * 2)) {
+        test1();
+        System.out.println("======");
+        test2();
+    }
+
+    public static void test1() {
+        String i = "1";
+        for (; !i.equals("8"); ) {
+            System.out.println(i);
+            i = String.valueOf(Integer.parseInt(i) * 2);
+        }
+        System.out.println("end" + i);
+    }
+
+    public static void test2() {
+        String i = "1";
+        for (; !i.equals("8"); i = String.valueOf(Integer.parseInt(i) * 2)) {
             System.out.println(i);
         }
-
+        System.out.println("end" + i);
     }
 }
