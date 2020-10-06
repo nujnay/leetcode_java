0cde0fa98ffd8229d06e6a286b0dd65dbba47797
yanjun <1292015395@qq.com>
2020-10-06 09:20:37 +0800

    master

--- src/leetcodenew/array/leetcode0268.java
+++ src/leetcodenew/array/leetcode0268.java
@@ -1,4 +1,41 @@
 package leetcodenew.array;
 
+/**
+ * given an array numbers containing n distinct numbers in the range [ 0, n]
+ * , return the only number in the range that is missing from the array .
+ * follow up: could you implement a solution using only o(1) extra space conplecity and o(n)
+ * runtime complexity?
+ */
 public class leetcode0268 {
+    public int missingNumber(int[] nums) {
+
+        int length = 0;
+        boolean thereAreZero = false;
+        int[] array = new int[0];
+        for (int i : nums) {
+            if (i + 1 > length) {
+                length = i + 1;
+                array = new int[i + 1];
+            }
+            if (i == 0) {
+                thereAreZero = true;
+            }
+            array[i] = i;
+        }
+        int empty = -1;
+        for (int i2 = 0; i2 < array.length ;i2++) {
+            if (i2 != 0) {
+                if (array[i2] == 0) {
+                    empty = i2;
+                }
+            }
+        }
+        if (thereAreZero) {
+            return empty;
+        } else {
+            return 0;
+        }
+        //first I need sort it, what should I sort it
+        //I need dynamic generate a array
+    }
 }
