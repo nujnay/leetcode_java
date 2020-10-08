9787a07c8018cb00211b98124012abece6271dfc
yanjun <1292015395@qq.com>
2020-10-08 22:15:36 +0800

    master

--- src/leetcodenew/array/leetcode0078.java
+++ src/leetcodenew/array/leetcode0078.java
@@ -1,5 +1,7 @@
 package leetcodenew.array;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,44 +11,15 @@ import java.util.List;
  */
 
 public class leetcode0078 {
-    //4个数 = c41 c42 c43 c44
-    //5个数 = c51 c52 c53 c54 c55
 
     public static List<List<Integer>> subsets(int[] nums) {
-        //先求出c length2 之后所有的数就都出来了
-        int[][] array2 = combination2(nums);
-        int[][] arrayAll = new int[2][2];
-        if (nums.length >= 3) {
-            for (int i = 3; i < nums.length; i++) {
-
-            }
+        List<List<Integer>> arrayAll = new ArrayList<>();
+        int[][][] tmp = null;
+        for (int i = 0; i < nums.length; i++) {
+            tmp = new int[nums.length - i][][ 0 + 2];
         }
-
     }
 
-    public static int[][] combination2(int[] nums) {
-        int arraySumLength = 0;
-        for (int i = nums.length - 1; i > 0; i--) {
-            arraySumLength += i;
-        }
-        int[][] arrays = new int[arraySumLength][2];
-        int[] tmpArray = null;
-        int point = 0;
-        // 4 number 0 1 2 3
-        // 0    1   2   3   4   5
-        // 01   02  03  12  13  23
-        int position = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            point = nums[i];
-            tmpArray = new int[nums.length - i - 1];
-            System.arraycopy(nums, i + 1, tmpArray, 0, nums.length - 1 - i);
-            for (int j = 0; j < tmpArray.length; j++) {
-                arrays[position] = new int[]{point, tmpArray[j]};
-                position++;
-            }
-        }
-        return arrays;
-    }
 
     //if number equal 2
     //拿出一个数组来 直到数组长度等于2
@@ -61,7 +34,7 @@ public class leetcode0078 {
 
     //5 number 3
     //0 1 2 3 4
-    //012 023 024 034 (0 + C52出了0开头的)
+    //012 013 014 023 024 034 (0 + C52出了0开头的)
     //123 124 134       (1 + C52出了0,1开头的)
     //234               (2 + C52出了0,1,2开头的)
 
@@ -86,5 +59,30 @@ public class leetcode0078 {
             }
             System.out.println(stringBuffer.toString());
         }
+    }
+
+
+    public static int[][] combination2(int[] nums) {
+        int arraySumLength = 0;
+        for (int i = nums.length - 1; i > 0; i--) {
+            arraySumLength += i;
+        }
+        int[][] arrays = new int[arraySumLength][2];
+        int[] tmpArray = null;
+        int point = 0;
+        // 4 number 0 1 2 3
+        // 0    1   2   3   4   5
+        // 01   02  03  12  13  23
+        int position = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            point = nums[i];
+            tmpArray = new int[nums.length - i - 1];
+            System.arraycopy(nums, i + 1, tmpArray, 0, nums.length - 1 - i);
+            for (int j = 0; j < tmpArray.length; j++) {
+                arrays[position] = new int[]{point, tmpArray[j]};
+                position++;
+            }
+        }
+        return arrays;
     }
 }
