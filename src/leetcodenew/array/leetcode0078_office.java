37a35fb9bfa7ed94d5ecd3d36c53b64409c71e23
yanjun <1292015395@qq.com>
2020-10-12 20:56:01 +0800

    master

--- src/leetcodenew/array/leetcode0078_office.java
+++ src/leetcodenew/array/leetcode0078_office.java
@@ -49,8 +49,9 @@ public class leetcode0078_office {
         }
     }
 
+
     public static void main(String[] args) {
-        int a = 0B100;
+        int a = 0B01;
         int b = a << 0b01;
         int c = a << 0b10;
         int d = a << 0b11;
