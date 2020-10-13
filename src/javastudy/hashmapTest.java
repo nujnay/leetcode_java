a81583f8ca70f3d1bce27adeddae38608fb001c0
yanjun <1292015395@qq.com>
2020-10-13 19:11:40 +0800

    master

--- src/javastudy/hashmapTest.java
+++ src/javastudy/hashmapTest.java
@@ -1,27 +1,44 @@
 package javastudy;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class hashmapTest {
     public static void main(String[] args) {
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("", "");
-        String test = new String(new char[]{1});
-        String test2 = "1";
-        int h = 0;
-        for (byte v : test.getBytes()) {
-            System.out.println("v:::" + v);
-            System.out.println("v2:::" + (v & 0xff));
-            h = 31 * h + (v & 0xff);
-            System.out.println("h:::" + h);
+//        HashMap<String, String> hashMap = new HashMap<>();
+//        hashMap.put("", "");
+//        String test = new String(new char[]{1});
+//        String test2 = "1";
+//        int h = 0;
+//        for (byte v : test.getBytes()) {
+//            System.out.println("v:::" + v);
+//            System.out.println("v2:::" + (v & 0xff));
+//            h = 31 * h + (v & 0xff);
+//            System.out.println("h:::" + h);
+//        }
+//
+//
+//        int hash = test.hashCode();
+//        System.out.println("hash::" + hash);
+//        int hash2 = test2.hashCode();
+//        System.out.println("hash2::" + hash2);
+//        int hashfinal = hash ^ (hash >>> 16);
+//        System.out.println("hashfinal::" + hashfinal);
+        test2();
+    }
+
+
+    public static void test2() {
+        Map<String, Set<String>> registerWords = new HashMap<>();
+        String platforms = "rr";
+        Set<String> set = registerWords.get(platforms);
+        if (CollectionUtils.isEmpty(set)) {
+            set = new HashSet<>();
+            registerWords.put(platforms, set);
         }
-
-
-        int hash = test.hashCode();
-        System.out.println("hash::" + hash);
-        int hash2 = test2.hashCode();
-        System.out.println("hash2::" + hash2);
-        int hashfinal = hash ^ (hash >>> 16);
-        System.out.println("hashfinal::" + hashfinal);
     }
 }
