d301f028ed0a2caed9244b8192d958bca3957703
yanjun <1292015395@qq.com>
2020-10-20 21:40:15 +0800

    master

--- src/leetcodenew/linkedlist/leetcode0206.java
+++ src/leetcodenew/linkedlist/leetcode0206.java
@@ -1,5 +1,27 @@
 package leetcodenew.linkedlist;
 
-public class leetcode0206 {
+import leetcodenew.ListNode;
 
+public class leetcode0206 {
+    /**
+     * Reverse a singly linked list.
+     * 1 -> 2 -> 3 -> 4 -> 5 -> null
+     *
+     * 3 -> 4 -> 5 -> null
+     *
+     *
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode tmp = head;
+        ListNode result = null;
+        while (tmp.next != null) {
+            result = tmp.next;
+            result.next = tmp;
+        }
+    }
 }
