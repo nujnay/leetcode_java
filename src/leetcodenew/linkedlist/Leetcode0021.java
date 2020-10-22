e192b9c5192b7bb60c54f6e070d83c48592fc098
yanjun <1292015395@qq.com>
2020-10-22 11:38:15 +0800

    master

---
+++ src/leetcodenew/linkedlist/Leetcode0021.java
@@ -0,0 +1,18 @@
+package leetcodenew.linkedlist;
+
+import leetcodenew.ListNode;
+
+public class Leetcode0021 {
+    /**
+     * 21. Merge Two Sorted Lists
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    }
+}
