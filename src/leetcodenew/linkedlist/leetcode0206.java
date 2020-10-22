e93d0fcb21edc2df93dbae1e55b402ba7f418925
yanjun <1292015395@qq.com>
2020-10-22 11:17:19 +0800

    master

--- src/leetcodenew/linkedlist/leetcode0206.java
+++ src/leetcodenew/linkedlist/leetcode0206.java
@@ -14,13 +14,17 @@ public class leetcode0206 {
      *
      */
     public static void main(String[] args) {
-        ListNode root = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+//        ListNode root = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+        ListNode root = new ListNode(1,  null);
         System.out.println(root);
         ListNode result = reverseList(root);
         System.out.println(result);
     }
 
     public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
         ListNode origin = head;
         ListNode result = null;
         while (origin.next != null) {
