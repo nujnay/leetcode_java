2ca9c8edad747719bafd2fcf167f99d4b06b9892
yanjun <1292015395@qq.com>
2020-10-22 11:38:46 +0800

    master

--- src/leetcodenew/linkedlist/Leetcode0021.java
+++ src/leetcodenew/linkedlist/Leetcode0021.java
@@ -5,6 +5,9 @@ import leetcodenew.ListNode;
 public class Leetcode0021 {
     /**
      * 21. Merge Two Sorted Lists
+     * Merge two sorted linked lists and return it as a new sorted list.
+     * The new list should be made by splicing together the nodes of the first two lists.
+     *
      *
      */
     public static void main(String[] args) {
