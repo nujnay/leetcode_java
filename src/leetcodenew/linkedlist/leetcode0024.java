a3ae3a8ceb71d7136f45244ad69913ec34b9ad85
yanjun <1292015395@qq.com>
2020-10-25 20:44:20 +0800

    master

---
+++ src/leetcodenew/linkedlist/leetcode0024.java
@@ -0,0 +1,24 @@
+package leetcodenew.linkedlist;
+
+import leetcodenew.ListNode;
+
+public class leetcode0024 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes. Only nodes itself may be changed.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+    }
+}
