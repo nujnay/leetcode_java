13d615d507bd3ba70a1e85f5fe922ba53733cb57
yanjun <1292015395@qq.com>
2020-10-25 13:21:14 +0800

    master

---
+++ src/leetcodenew/linkedlist/leetcode0002.java
@@ -0,0 +1,26 @@
+package leetcodenew.linkedlist;
+
+import leetcodenew.ListNode;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+ * and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+public class leetcode0002 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
