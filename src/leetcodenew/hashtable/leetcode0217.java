cf62e65571c82427b647ada048bb568c334f7cbb
yanjun <1292015395@qq.com>
2020-10-27 16:12:47 +0800

    master

--- src/leetcodenew/hashtable/leetcode0217.java
+++ src/leetcodenew/hashtable/leetcode0217.java
@@ -4,6 +4,16 @@ public class leetcode0217 {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * Given an array of integers, find if the array contains any duplicates.
+     *
+     * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/contains-duplicate
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     class Solution {
         public boolean containsDuplicate(int[] nums) {
 
