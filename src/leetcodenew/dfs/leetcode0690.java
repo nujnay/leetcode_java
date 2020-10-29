f51fd695b0fd3e27d2d8eeb4bf6c8459fb109374
yanjun <1292015395@qq.com>
2020-10-29 15:25:50 +0800

    master

--- src/leetcodenew/dfs/leetcode0690.java
+++ src/leetcodenew/dfs/leetcode0690.java
@@ -15,7 +15,8 @@ public class leetcode0690 {
      * Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
      * Output: 11
      * Explanation:
-     * Employee 1 has importance value 5, and he has two direct subordinates: employee 2 and employee 3. They both have importance value 3. So the total importance value of employee 1 is 5 + 3 + 3 = 11.
+     * Employee 1 has importance value 5, and he has two direct subordinates: employee 2 and employee 3.
+     * They both have importance value 3. So the total importance value of employee 1 is 5 + 3 + 3 = 11.
      *
      *
      * Note:
