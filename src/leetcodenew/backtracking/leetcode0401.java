9b3fb44c47f19efd70f5de00f63cc20d8ccab16d
yanjun <1292015395@qq.com>
2020-11-06 16:54:57 +0800

    master

--- src/leetcodenew/backtracking/leetcode0401.java
+++ src/leetcodenew/backtracking/leetcode0401.java
@@ -1,5 +1,6 @@
 package leetcodenew.backtracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class leetcode0401 {
@@ -26,12 +27,82 @@ public class leetcode0401 {
      */
 
     public static void main(String[] args) {
-        class Solution {
-            public List<String> readBinaryWatch(int num) {
 
+    }
 
+    /**
+     * 方法2:回溯,二叉树(官方喜欢的选和不选)
+     * 使用一个数组来保存手表上的数组,其中前四位1,2,4,8表示是小时,后面6位01,02,04,08,16,32表示的是分钟;
+     */
+    class Solution {
 
+        // 下标索引在0-3范围内的表示的是小时,不在这个范围内的表示的是分钟;
+        Integer[] watch = {1, 2, 4, 8, 1, 2, 4, 8, 16, 32};
+
+        List<String> res = new ArrayList<>();
+
+        public List<String> readBinaryWatch(int num) {
+            dfs(num, 0, 0, 0, 0);
+            return res;
+        }
+
+        /**
+         * @param num    题目要求用的总的数字个数;
+         * @param idx    当前watch数组中用到数字的索引
+         * @param count  当前已经用到的数字个数
+         * @param hour   当前已经算出来的小时
+         * @param minute 当前已经算出来的分钟
+         */
+        private void dfs(int num, int idx, int count, int hour, int minute) {
+
+            // 剪枝;
+            if (hour > 11 || minute > 59) {
+                // 这些结果是要丢弃的;
+                return;
             }
+
+            // 凑足了数字,则需要添加结果;
+            if (count == num) {
+                StringBuffer sb = new StringBuffer();
+                // 小时不会以零开头，比如 “01:00” 是不允许的，应为 “1:00”。
+                sb.append(hour).append(":");
+                // 分钟必须由两位数组成，可能会以零开头，比如 “10:2” 是无效的，应为 “10:02”。
+                if (minute < 10) {
+                    sb.append("0").append(minute);
+                } else {
+                    sb.append(minute);
+                }
+                res.add(sb.toString());
+                return;
+            }
+
+            // 如果不用for循环,则需要自己添加递归终止判定;
+            if (idx >= watch.length) {
+                return;
+            }
+
+
+            // 不选;不选数字则count不需要加1;
+            dfs(num, idx + 1, count, hour, minute);
+
+            // 选;
+
+            if (idx <= 3) { // 加到小时上;
+                hour += watch[idx];
+            } else {
+                minute += watch[idx];
+            }
+
+            dfs(num, idx + 1, count + 1, hour, minute);
+
+            if (idx <= 3) {
+                hour -= watch[idx];
+            } else {
+                minute -= watch[idx];
+            }
+
+
+            return;
         }
     }
 }
