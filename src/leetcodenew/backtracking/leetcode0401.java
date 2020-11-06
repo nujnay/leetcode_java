7f32ab4402b6f0a6321c0c5bd2a5c85eaab3d0d4
yanjun <1292015395@qq.com>
2020-11-06 14:21:12 +0800

    master

--- src/leetcodenew/backtracking/leetcode0401.java
+++ src/leetcodenew/backtracking/leetcode0401.java
@@ -22,6 +22,7 @@ public class leetcode0401 {
      * The order of output does not matter.
      * The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
      * The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, it should be "10:02".
+     *
      */
 
     public static void main(String[] args) {
