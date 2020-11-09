1ca2d776ab6d610c63f9318c81c30e422edeb060
yanjun <1292015395@qq.com>
2020-11-09 14:16:47 +0800

    master

---
+++ src/javastudy/ListTest.java
@@ -0,0 +1,16 @@
+package javastudy;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ListTest {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("");
+        strings.indexOf("0");
+        LinkedList<String> strings1 = new LinkedList<>();
+        strings1.add("");
+        strings1.indexOf("0");
+    }
+}
---
+++ src/javastudy/SetTest.java
@@ -0,0 +1,11 @@
+package javastudy;
+
+import java.util.HashSet;
+
+public class SetTest {
+    public static void main(String[] args) {
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("");
+        hashSet.contains("");
+    }
+}
